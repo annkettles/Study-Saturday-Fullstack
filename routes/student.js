2e4e23b312185c98291d8837ae294e535a71d716
Ann Kettles <ann.a.kettles@gmail.com>
2019-03-23 10:33:56 -0400

    completed cycle 0

--- routes/student.js
+++ routes/student.js
@@ -3,41 +3,48 @@ const Student = require('../db/models/students');
 const Test = require('../db/models/tests');
 
 router.get('/:studentId', function(req, res, next) {
-  Student.findById(req.params.studentId)
-    .then(student => {
-      if (!student) return res.sendStatus(404);
-      res.json(student);
-    })
-    .catch(next);
+	Student.findById(req.params.studentId)
+		.then((student) => {
+			if (!student) return res.sendStatus(404);
+			res.json(student);
+		})
+		.catch(next);
 });
 
 router.get('/', function(req, res, next) {
-  Student.findAll({ include: { all: true } }).then(students =>
-    res.json(students)
-  );
+	Student.findAll({include: {all: true}}).then((students) => res.json(students));
+});
+
+router.post('/', async (req, res, next) => {
+	try {
+		const newStudent = await Student.create(req.body);
+		res.json(newStudent);
+	} catch (err) {
+		next(err);
+	}
 });
 
 router.put('/:id', function(req, res, next) {
-  Student.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-    returning: true,
-  })
-    .then(test => res.status(201).json(test[1][0]))
-    .catch(next);
+	Student.update(req.body, {
+		where: {
+			id: req.params.id
+		},
+		returning: true
+	})
+		.then((test) => res.status(201).json(test[1][0]))
+		.catch(next);
 });
 
 router.delete('/:id', function(req, res, next) {
-  Student.destroy({
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then(() => {
-      res.sendStatus(204);
-    })
-    .catch(next);
+	Student.destroy({
+		where: {
+			id: req.params.id
+		}
+	})
+		.then(() => {
+			res.sendStatus(204);
+		})
+		.catch(next);
 });
 
 module.exports = router;
